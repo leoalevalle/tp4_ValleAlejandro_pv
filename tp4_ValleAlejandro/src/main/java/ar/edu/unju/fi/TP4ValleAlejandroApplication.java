ac1303fa2273270e824986a438e5134a71258c11
usuario <usuario@DESKTOP-2FH21K9>
2024-06-08 11:14:34 -0300

    Correciones parte 1.

---
+++ tp4_ValleAlejandro/src/main/java/ar/edu/unju/fi/TP4ValleAlejandroApplication.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TP4ValleAlejandroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TP4ValleAlejandroApplication.class, args);
+	}
+}
